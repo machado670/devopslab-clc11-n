1a933c52ac20bab60a4c8b4fe15eb4c0bc140f49
machado670 <43648762+machado670@users.noreply.github.com>
2024-03-12 12:24:03 -0300

    Update test.py

--- test.py
+++ test.py
@@ -16,4 +16,4 @@ class Test(unittest.TestCase):
 
     def test_conteudo(self):
         # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Cicero Edson Machado v1.0")
+        self.assertEqual(self.result.data.decode('utf-8'), "Hello Cicero Edson Machado")
